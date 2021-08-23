de5592c06de4cf59aafff495f90988f47aa4be09
milcab <milcarodriguez23@gmail.com>
2021-08-23 19:21:55 -0400

    exam 1

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Milca Brito" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
@@ -13,11 +13,75 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
+document.getElementById('add-gb').addEventListener('click', function () {
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+    gb++;
+    const quantity = document.querySelector('#qty-gb');
+    quantity.innerHTML = gb;
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
+})
+
+document.getElementById('minus-gb').addEventListener('click', function () {
+    // HINT: You can delete this console.log after you no longer need it!
+
+    if (gb > 0) {
+        gb--;
+        const quantity = document.querySelector('#qty-gb');
+        quantity.innerHTML = gb;
+    }
+
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
+})
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.getElementById('add-cc').addEventListener('click', function () {
+    // HINT: You can delete this console.log after you no longer need it!
+    cc++;
+    const quantity = document.querySelector('#qty-cc');
+    quantity.innerHTML = cc;
+
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
+})
+
+document.getElementById('minus-cc').addEventListener('click', function () {
+    // HINT: You can delete this console.log after you no longer need it!
+
+    if (cc > 0) {
+        cc--;
+        const quantity = document.querySelector('#qty-cc');
+        quantity.innerHTML = cc;
+    }
+
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
+})
+
+// Event listener for clicks on the "+" button for Ginger Bread cookies
+document.getElementById('add-sugar').addEventListener('click', function () {
+    // HINT: You can delete this console.log after you no longer need it!
+    sugar++;
+    const quantity = document.querySelector('#qty-sugar');
+    quantity.innerHTML = sugar;
+
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function () {
+    // HINT: You can delete this console.log after you no longer need it!
+
+    if (sugar > 0) {
+        sugar--;
+        const quantity = document.querySelector('#qty-sugar');
+        quantity.innerHTML = sugar;
+    }
+
+    const total = document.querySelector('#qty-total');
+    total.innerHTML = gb + cc + sugar;
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
